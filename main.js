9434635b59a45e1877b161ec856150783d811a9c
anaquaglio <163136888+anaquaglio@users.noreply.github.com>
2024-03-12 09:39:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4f3c3efadea01c68b7ea2f2cbbff8fc8~c5_720x720.jpeg?lk3s=a5d48078&x-expires=1709658000&x-signature=t5593TeG1DTDRmEVcLvEpXVOXhM%3D";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://pbs.twimg.com/media/F31n-o9WEAMC5E_?format=jpg&name=large"
+}
+
